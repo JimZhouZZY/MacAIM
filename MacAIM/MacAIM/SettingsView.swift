1007cc0e08744d2410adece9a212d57157ae4bbf
Jim Zhou <jimzhouzzy@gmail.com>
2025-03-21 01:01:44 +0800

    v1.1.0-beta.2: contact section of settings

--- MacAIM/MacAIM/SettingsView.swift
+++ MacAIM/MacAIM/SettingsView.swift
@@ -11,24 +11,39 @@ struct SettingsView: View {
             List {
                 Section(header: Text("General Preferences").font(.headline)) {
                     Toggle("Start at login", isOn: $startAtLogin)
+                        .padding(.vertical, 3)
                     Toggle("Silent start", isOn: $silentStart)
+                        .padding(.vertical, 3)
                     Toggle("Debug mode", isOn: $debugMode)
+                        .padding(.vertical, 3)
                     Toggle("Status icon", isOn: $showStatusBarIcon)
+                        .padding(.vertical, 3)
                 }
                 .padding(.horizontal, 10)
                 
-                //Section {
-                //    Button("Close") {
-                //        NSApp.keyWindow?.close()
-                //    }
-                //    .frame(maxWidth: .infinity, alignment: .center)
-                //    .padding(.vertical, 0)
-                //}
+                Section(header: Text("Contact").font(.headline)) {
+                    Button("About") {
+                        if let url = URL(string: "https://github.com/JimZhouZZY/MacAIM") {
+                            NSWorkspace.shared.open(url)
+                        }
+                    }
+                    .padding(.vertical, 1)
+                    .padding(.horizontal, 20)
+                    Button("Report Issues") {
+                        if let url = URL(string: "https://github.com/JimZhouZZY/MacAIM/issues") {
+                            NSWorkspace.shared.open(url)
+                        }
+                    }
+                    .padding(.vertical, 1)
+                    .padding(.horizontal, 20)
+                }
+                .padding(.horizontal, 10)
             }
             .navigationTitle("Settings")
-            .frame(width: 320, height: 250)
-            .frame(width: 320, height: 250)
-            .fixedSize() // fix window size
+            .frame(width: 320, height: 320)
+            .fixedSize()
         }
+        .frame(width: 320, height: 320)
+        .fixedSize()
     }
 }
